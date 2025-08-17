e386456ac6fe5b83bf0c3faddcf7ae1c1a501762
GQR <gaoqirun8@gmail.com>
2025-08-17 11:10:33 +0200

    Implement training for Go2

---
+++ legged_gym/legged_gym/envs/go2/go2_config.py
@@ -0,0 +1,82 @@
+# SPDX-FileCopyrightText: Copyright (c) 2021
+# SPDX-License-Identifier: BSD-3-Clause
+
+from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
+
+class Go2RoughCfg(LeggedRobotCfg):
+    class init_state(LeggedRobotCfg.init_state):
+        # Initial pose (can be tuned)
+        pos = [0.0, 0.0, 0.42]  # x,y,z [m]
+        default_joint_angles = {  # = target angles [rad] when action = 0.0
+            # Hips
+            'FL_hip_joint':  0.10,   # [rad]
+            'RL_hip_joint':  0.10,   # [rad]
+            'FR_hip_joint': -0.10,   # [rad]
+            'RR_hip_joint': -0.10,   # [rad]
+            # Thighs
+            'FL_thigh_joint': 1.00,  # [rad]
+            'RL_thigh_joint': 1.00,  # [rad]
+            'FR_thigh_joint': 1.00,  # [rad]
+            'RR_thigh_joint': 1.00,  # [rad]
+            # Calves
+            'FL_calf_joint': -1.50,  # [rad]
+            'RL_calf_joint': -1.50,  # [rad]
+            'FR_calf_joint': -1.50,  # [rad]
+            'RR_calf_joint': -1.50,  # [rad]
+        }
+
+    class init_state_slope(LeggedRobotCfg.init_state):
+        # Alternative start on slope/ramps (optional)
+        pos = [0.56, 0.0, 0.24]  # x,y,z [m]
+        default_joint_angles = {  # = target angles [rad] when action = 0.0
+            'FL_hip_joint':  0.03,   # [rad]
+            'RL_hip_joint':  0.03,   # [rad]
+            'FR_hip_joint': -0.03,   # [rad]
+            'RR_hip_joint': -0.03,   # [rad]
+
+            'FL_thigh_joint': 1.00,  # [rad]
+            'RL_thigh_joint': 1.90,  # [rad]
+            'FR_thigh_joint': 1.00,  # [rad]
+            'RR_thigh_joint': 1.90,  # [rad]
+
+            'FL_calf_joint': -2.20,  # [rad]
+            'RL_calf_joint': -0.90,  # [rad]
+            'FR_calf_joint': -2.20,  # [rad]
+            'RR_calf_joint': -0.90,  # [rad]
+        }
+
+    class control(LeggedRobotCfg.control):
+        # PD Drive parameters:
+        control_type = 'P'
+        stiffness = {'joint': 30.0}   # [N*m/rad]
+        damping   = {'joint': 0.6}    # [N*m*s/rad]
+        # action scale: target angle = action_scale * action + defaultAngle
+        action_scale = 0.25
+        # decimation: number of control action updates @ sim DT per policy DT
+        decimation  = 4
+
+    class asset(LeggedRobotCfg.asset):
+        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/go2/urdf/go2.urdf'
+        foot_name = "foot"
+        penalize_contacts_on = ["thigh", "calf"]
+        terminate_after_contacts_on = ["base"]  # add more links if needed
+        self_collisions = 1  # 1 to disable, 0 to enable... bitwise filter
+
+    class rewards(LeggedRobotCfg.rewards):
+        soft_dof_pos_limit = 0.9
+        base_height_target = 0.27
+        # class scales(LeggedRobotCfg.rewards.scales):
+        #     torques = -0.0002
+        #     dof_pos_limits = -10.0
+
+    class sim(LeggedRobotCfg.sim):
+        dt = 0.005
+        substeps = 1
+        # Use project defaults for remaining PhysX/contact parameters
+
+class Go2RoughCfgPPO(LeggedRobotCfgPPO):
+    class algorithm(LeggedRobotCfgPPO.algorithm):
+        entropy_coef = 0.01
+    class runner(LeggedRobotCfgPPO.runner):
+        run_name = ''
+        experiment_name = 'rough_go2'
